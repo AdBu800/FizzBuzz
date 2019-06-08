9708e993e1b6fb2797f4437a7cface840c0962bf
Adam Buck <adam.buck@jumbo.com>
2019-06-08 08:24:24 +0200

    first commit

---
+++ FizzBuzz/DivisibilityChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class DivisibilityChecker
+    {
+        public Dictionary<int, string> NumberStringMapping { get; private set; }
+
+        public DivisibilityChecker(Dictionary<int, string> numberStringMapping)
+        {
+            NumberStringMapping = numberStringMapping;
+        }
+
+        public string EvenlyDivisable(int number)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(var kvp in NumberStringMapping)
+            {
+                if(number % kvp.Key == 0)
+                {
+                    sb.Append(kvp.Value);
+                }
+            }
+
+            if(sb.Length != 0)
+            {
+                sb.AppendLine(string.Format(" ({0})", number));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Enumerable.Range(1, 100).ToList();
+
+            DivisibilityChecker dc = new DivisibilityChecker(new Dictionary<int, string>
+            {
+                { 3, "Fizz" },
+                { 5, "Buzz" },
+                { 7, "Jazz" },
+            });
+
+            foreach (var number in numbers)
+            {
+                Console.Write(dc.EvenlyDivisable(number));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
